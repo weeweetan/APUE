3921bcef0cff9d49652451da8dfc4b3b6e8c306a
tanwei <1113513062@qq.com>
2020-12-20 17:11:43 +0800

    14-1.c~14-5.c

---
+++ 14-2.c
@@ -0,0 +1,17 @@
+//
+// Created by 11135 on 2020/12/20.
+//
+#include "apue.h"
+#include <fcntl.h>
+
+int lock_reg(int fd, int cmd, int type, off_t offset, int whence, off_t len)
+{
+    struct flock lock;
+
+    lock.l_type = type;
+    lock.l_start = offset;
+    lock.l_whence = whence;
+    lock.l_len = len;
+
+    return (fcntl(fd, cmd, &lock));
+}
