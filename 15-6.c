d1751698393020a639f7c0811e848514a7d14cec
tanwei <1113513062@qq.com>
2021-01-22 22:41:11 +0800

    15-6.c

---
+++ 15-6.c
@@ -0,0 +1,29 @@
+//
+// Created by 11135 on 2021/1/18.
+//
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+    char    line[MAXLINE];
+    FILE    *fpin;
+
+    if ((fpin = popen("myuclc", "r")) == NULL)
+        err_sys("popen error");
+    for (;;)
+    {
+        fputs("prompt> ", stdout);
+        fflush(stdout);
+        if (fgets(line, MAXLINE, fpin) == NULL)
+            break;
+        if (fputs(line, stdout) == EOF)
+            err_sys("fputs error to pipe");
+    }
+
+    if (pclose(fpin) == -1)
+        err_sys("pclose error");
+    putchar('\n');
+    exit(0);
+
+}
