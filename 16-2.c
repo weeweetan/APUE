8e30e057eb35e8fb27d304c20b957a3e236d68de
tanwei <1113513062@qq.com>
2021-01-27 22:05:03 +0800

    16-2.c

---
+++ 16-2.c
@@ -0,0 +1,24 @@
+//
+// Created by 11135 on 2021/1/23.
+//
+#include "apue.h"
+#include <sys/socket.h>
+
+#define MAXSLEEP 128
+
+int connect_retry(int sockfd, const struct sockaddr *addr, socklen_t alen)
+{
+    int numsec;
+
+    for (numsec = 1; numsec <= MAXSLEEP; numsec<<=1) {
+        if (connect(sockfd, addr, alen) == 0)
+        {
+            return(0);
+        }
+
+        if (numsec <= MAXSLEEP/2)
+            sleep(numsec);
+    }
+
+    return(-1);
+}
