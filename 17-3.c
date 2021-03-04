dd13cda628e0e3acd369e2b50e18127905a0cb01
tanwei <1113513062@qq.com>
2021-03-04 22:27:03 +0800

    17-3.c

---
+++ 17-3.c
@@ -0,0 +1,22 @@
+//
+// Created by 11135 on 2021/1/24.
+//
+#include "apue.h"
+#include <sys/socket.h>
+#include <sys/un.h>
+
+int main(int argc, char **argv)
+{
+    int fd, size;
+    struct sockaddr_un un;
+
+    un.sun_family = AF_UNIX;
+    strcpy(un.sun_path, "foo.socket");
+    if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+        err_sys("socket failed");
+    size = offsetof(struct sockaddr_un, sun_path) + strlen(un.sun_path);
+    if (bind(fd, (struct sockaddr *)&un, size) < 0)
+        err_sys("bind failed");
+    printf("UNIX domain socket bound\n");
+    exit(0);
+}
